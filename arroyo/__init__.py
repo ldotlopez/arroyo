a7d64dbeae351c0481f4543aa1b714b334acaf3c
L. López <ldotlopez@gmail.com>
2014-10-16 19:35:58 +0200

    Remove duplicated exception

--- arroyo/__init__.py
+++ arroyo/__init__.py
@@ -1,11 +1,3 @@
-class ArgumentError(Exception):
-    def __init__(self, msg, *args, tip=True, **kwargs):
-        if not msg.endswith('.'):
-            msg += "."
-        msg += " Try -h/--help switch for more information"
-        super(ArgumentError, self).__init__(msg, *args, **kwargs)
-
-
 class SourceNotFound(Exception):
     pass
 
