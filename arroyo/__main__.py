8f284480c97a43e517a5758059df453988e438e4
L. López <ldotlopez@gmail.com>
2014-12-30 17:21:59 +0100

    Move more code out of app.py

--- arroyo/__main__.py
+++ arroyo/__main__.py
@@ -5,6 +5,18 @@ if __name__ == '__main__':
     import sys
     import arroyo.exc
     from arroyo.app import app
+
+    extensions = {
+        'importers': ('eztv', 'spanishtracker', 'thepiratebay'),
+        'selectors': ('source', 'episode'),
+        'commands': ('analyze', 'db', 'downloads', 'mediainfo', 'search'),
+        'downloaders': ('mock', 'transmission')
+    }
+
+    for (k, v) in extensions.items():
+        exts = ["%s.%s" % (k, e) for e in v]
+        app.load_extension(*exts)
+
     try:
         app.run()
     except arroyo.exc.ArgumentError as e:
--- arroyo/app.py
+++ arroyo/app.py
@@ -10,14 +10,3 @@ def argument(*args, **kwargs):
 
 
 app = Arroyo()
-
-extensions = {
-    'importers': ('eztv', 'spanishtracker', 'thepiratebay'),
-    'selectors': ('source', 'episode'),
-    'commands': ('analyze', 'db', 'downloads', 'mediainfo', 'search'),
-    'downloaders': ('mock', 'transmission')
-}
-
-for (k, v) in extensions.items():
-    exts = ["%s.%s" % (k, e) for e in v]
-    app.load_extension(*exts)
