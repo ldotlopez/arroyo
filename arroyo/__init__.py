50ba7a5c7880e88bd5eea315d6d5732b7cbb00cf
Luis López <lopezl@uji.es>
2015-06-23 13:07:01 +0200

    Migrate Query to InmutableDict as QuerySpec

--- arroyo/__init__.py
+++ arroyo/__init__.py
@@ -1,15 +1,15 @@
 __all__ = [
     'Arroyo',
-    'Importer',
+    'Importer', 'OriginSpec'
     'Downloader',
     'Mediainfo',
-    'Selector', 'Query',
+    'Selector', 'QuerySpec',
     'Signaler'
 ]
 
 from .core import Arroyo
-from .importer import Importer
+from .importer import Importer, OriginSpec
 from .downloader import Downloader
 from .mediainfo import Mediainfo
-from .selector import Selector, Query
+from .selector import Selector, QuerySpec
 from .signaler import Signaler
