6c79ed05bd5e80aaa51bde3849224c035be83929
Luis López <ldotlopez@gmail.com>
2015-07-02 09:07:13 +0200

    Port ep-src link code

--- arroyo/exts/queries/episode.py
+++ arroyo/exts/queries/episode.py
@@ -5,6 +5,25 @@ from arroyo import (
 
 
 class Query(exts.Query):
+    def __init__(self, app, spec):
+        super().__init__(app, spec)
+
+        self._known_srcs = set()
+        self.app.signals.connect('source-state-change',
+                                 self._on_source_state_change)
+
+
+    def _on_source_state_change(self, sender, **kwargs):
+        src = kwargs['source']
+        if src not in self._known_srcs:
+            return
+
+        # Link src and episode
+        ep = src.episode
+        ep.selection = models.EpisodeSelection(source=src)
+        self.app.db.session.commit()
+        self._known_srcs.remove(src)
+
     def matches(self, everything):
         q = self.app.db.session.query(models.Source).join(models.Episode)
 
@@ -21,14 +40,16 @@ class Query(exts.Query):
         if params == self.params:
             return []
 
+        items, g = itertools.tee(items)
+        self._known_srcs = set(list(g))
+
         return items
 
 
-# class Query(exts.Query):
-#     _SUPPORTED_Q = ('1080p', '720p', '480p', 'hdtv')
-#     _SUPPORTED_Q_STRING = ", ".join([
-#         "'{}'".format(x) for x in _SUPPORTED_Q
-#     ])
+#
+# Old stuff from previous architecture.
+# Keep here as a reference
+#
 
 #     def __init__(self, app, spec):
 #         super().__init__(app, spec)
@@ -57,67 +78,6 @@ class Query(exts.Query):
 #             return quality == screen_size
 #         else:
 #             return not screen_size and fmt == 'hdtv'
-
-#     def matches(self, everything):
-#         # Get various parameters
-#         series = self.spec.get('series')
-#         year = self.spec.get('year')
-#         language = self.spec.get('language')
-#         season = self.spec.get('season')
-#         number = self.spec.get('episode')
-
-#         # Basic checks
-#         if not series:
-#             raise exc.ArgumentError('series filter is required')
-
-#         # Parse quality filter
-#         quality = self.spec.get('quality', None)
-#         if quality:
-#             quality = quality.lower()
-#             if quality not in self.__class__._SUPPORTED_Q:
-#                 msg = (
-#                     "quality '{quality}' not supported, "
-#                     "only {supported_qualities} are supported"
-#                 )
-#                 msg = msg.format(
-#                     quality=quality,
-#                     supported_qualities=self.__class__._SUPPORTED_Q_STRING
-#                 )
-#                 raise exc.ArgumentError(msg)
-
-#         qs = self.app.db.session.query(models.Episode)
-
-#         # Strip episodes with a selection
-#         if not everything:
-#             qs = qs.filter(models.Episode.selection == None)  # nopep8
-
-#         if series:
-#             qs = qs.filter(models.Episode.series.ilike(series))
-
-#         if year:
-#             qs = qs.filter(models.Episode.year == year)
-
-#         if language:
-#             qs = qs.filter(models.Episode.language == language)
-
-#         if season:
-#             qs = qs.filter(models.Episode.season == season)
-
-#         if number:
-#             qs = qs.filter(models.Episode.number == number)
-
-#         ret = []
-#         for ep in qs:
-#             srcs = ep.sources
-#             if quality:
-#                 srcs = filter(lambda x: self.quality_filter(x, quality), srcs)
-
-#             ret += srcs
-
-#             for src in srcs:
-#                 self._source_table[src] = ep
-
-#         return ret
 
 #     def sort(self, srcs):
 #         # https://wiki.python.org/moin/HowTo/Sorting#The_Old_Way_Using_the_cmp_Parameter
