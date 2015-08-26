0a9a808117baf623ce8f2d4e07952a16a0a4a4d2
Luis López <ldotlopez@gmail.com>
2015-08-26 14:27:27 +0200

    Update API usage

--- arroyo/cron.py
+++ arroyo/cron.py
@@ -11,11 +11,11 @@ class CronManager:
         self._app = app
 
     def run_all(self, force=False):
-        for impl in self._app.get_implementations('crontask'):
+        for impl in self._app.get_implementations(CronTask):
             self.run(impl, force)
 
     def run(self, name, force=False):
-        task = self._app.get_extension('crontask', name)
+        task = self._app.get_extension(CronTask, name)
 
         if force or task.should_run:
             task.run()
