15047b3409dd888e687dfe5c84a1964d1f479c02
Luis López <ldotlopez@gmail.com>
2016-05-19 14:58:44 +0200

    Parse detail pages

--- arroyo/plugins/elitetorrent.py
+++ arroyo/plugins/elitetorrent.py
@@ -2,10 +2,12 @@
 
 from arroyo import plugin
 
+from datetime import datetime
 import re
-
+import time
 
 import bs4
+import humanfriendly
 from ldotcommons import utils
 
 
@@ -23,14 +25,29 @@ class EliteTorrent(plugin.Origin):
         'an': 60*60*24*365
     }
 
+    _categories = {
+        'series': 'episode',
+        'peliculas': 'movie'
+    }
+
     def paginate(self, url):
-        # yield from self.paginate_by_query_param(url, 'page', default=0)
-        yield url
+        if re.search(r'/torrent/\d+/'):
+            yield url
+        else:
+            yield url
 
     def get_query_url(self, query):
         return None
 
     def parse(self, buff):
+        soup = bs4.BeautifulSoup(buff, "html.parser")
+        info = soup.select_one('.info-tecnica')
+        if info:
+            return self.parse_detailed(soup)
+        else:
+            return self.parse_listing(soup)
+
+    def parse_listing(self, soup):
         def parse_row(row):
             r = {
                 'name': row.select_one('a.nombre').text,
@@ -71,10 +88,60 @@ class EliteTorrent(plugin.Origin):
 
             return r
 
-        soup = bs4.BeautifulSoup(buff, "html.parser")
         rows = soup.select('table.fichas-listado tr')[1:]
 
         return list(map(parse_row, rows))
+
+    def parse_detailed(self, soup):
+        info = soup.select_one('.info-tecnica')
+        name = soup.select_one('#box-ficha h2').text
+
+        if '(vose)' in name.lower():
+            lang = None
+        else:
+            lang = 'esp-es'
+
+        links = soup.select('.enlace_torrent')
+        links = map(lambda x: x.attrs['href'], links)
+        links = filter(lambda x: x.startswith('magnet:?'), links)
+        try:
+            uri = next(links)
+        except StopIteration:
+            return []
+
+        for ch in info.children:
+            try:
+                txt = ch.text
+            except AttributeError:
+                continue
+
+            if txt.lower() == 'fecha':
+                created = datetime.strptime(ch.next_sibling.text, '%d-%m-%Y')
+                created = int(time.mktime(created.timetuple()))
+
+            elif txt.lower().startswith('categor'):
+                cat = ch.next_sibling.text.lower()
+                type = self._categories.get(cat, None)
+
+            elif txt.lower().startswith('tama'):
+                size = humanfriendly.parse_size(ch.next_sibling.text)
+
+        m = re.search(r'semillas: (\d+) \| clientes: (\d+)',
+                      soup.select_one('.ppal').text.lower())
+        if m:
+            seeds = m.group(1)
+            leechers = m.group(2)
+
+        return [{
+            'name': name,
+            'uri': uri,
+            'size': size,
+            'type': type,
+            'language': lang,
+            'seeds': seeds,
+            'leechers': leechers
+        }]
+
 
 __arroyo_extensions__ = [
     ('elitetorrent', EliteTorrent)
