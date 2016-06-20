6f3cd9b415123c364d359165a70200938d406f0a
Luis López <ldotlopez@gmail.com>
2016-06-20 14:45:02 +0200

    Profiling stuff

---
+++ profile.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ "${D:0:1}" != "/" ]; then
+		D="$PWD/$D"
+fi
+
+if [ -z "$VIRTUAL_ENV" ]; then 
+	source "$D/env/bin/activate" 2>/dev/null || {
+		echo "expected virtual environment in '$D/env' not found"
+		exit 1
+	}
+fi
+
+PYTHONPATH="$D" python3 -m profile -o profile.pstats "$D/arroyo/__main__.py" \
+	--db-uri sqlite:///"$D/arroyo.db" \
+	--config-file "$D/arroyo.yml" \
+	"$@"
+
+PYTHONPATH="$D" gprof2dot -f pstats profile.pstats | dot -Tpng -o profile.png
