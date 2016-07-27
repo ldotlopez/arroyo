465b9633f4570179f9b041b0279c81dcd6d2da69
Luis López <ldotlopez@gmail.com>
2016-07-27 19:40:29 +0200

    [tests] Test parse method for Origin

--- tests/test_origins.py
+++ tests/test_origins.py
@@ -7,7 +7,7 @@ from arroyo import plugin
 import testapp
 
 
-class TestOrigin2:
+class TestOrigin:
     PARSE_TESTS = []
     PAGINATION_TESTS = []
     QUERY_TESTS = []
@@ -65,6 +65,19 @@ class TestOrigin2:
             self.assertEqual(collected, expected,
                              msg='Fail pagination for {}'.format(start))
 
+    def test_parse(self):
+        for (sample, n_expected) in self.PARSE_TESTS:
+            spec = plugin.OriginSpec(name='foo', backend=self.IMPLEMENTATION_NAME)
+            origin = self.app.importer.get_origin_for_origin_spec(spec)
+
+            with open(testapp.www_sample_path(sample)) as fh:
+                results = list(origin.parse(fh.read()))
+
+            self.assertEqual(
+                n_expected,len(results),
+                msg="Parse missmatch for {}".format(sample)
+            )
+
     def test_query_url(self):
         spec = plugin.OriginSpec(name='foo', backend=self.IMPLEMENTATION_NAME)
         origin = self.app.importer.get_origin_for_origin_spec(spec)
@@ -80,7 +93,7 @@ class TestOrigin2:
                 msg='Failed query for {}'.format(repr(query)))
 
 
-class EztvTest2(TestOrigin2, unittest.TestCase):
+class EztvTest(TestOrigin, unittest.TestCase):
     PLUGINS = ['eztv']
     IMPLEMENTATION_NAME = 'eztv'
 
@@ -154,7 +167,7 @@ class EztvTest2(TestOrigin2, unittest.TestCase):
             eztv.get_url_for_series(table, 'foo')
 
 
-class KATTest2(TestOrigin2, unittest.TestCase):
+class KATTest(TestOrigin, unittest.TestCase):
     PLUGINS = ['kickass']
     IMPLEMENTATION_NAME = 'kickass'
 
@@ -210,155 +223,155 @@ class KATTest2(TestOrigin2, unittest.TestCase):
     ]
 
 
-class TestOrigin:
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        warnings.warn("TestOrigin doesn't validate keys")
+# class TestOrigin:
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         warnings.warn("TestOrigin doesn't validate keys")
 
-    def setUp(self):
-        settings = {}
-        settings.update(
-            {'plugin.' + x + '.enabled': True
-             for x in self.PLUGINS}
-        )
-        self.app = testapp.TestApp(settings)
+#     def setUp(self):
+#         settings = {}
+#         settings.update(
+#             {'plugin.' + x + '.enabled': True
+#              for x in self.PLUGINS}
+#         )
+#         self.app = testapp.TestApp(settings)
 
-    def test_implementation(self):
-        impl = self.app.get_implementation(plugin.Origin, self.BACKEND)
-        self.assertTrue(
-            hasattr(impl, 'paginate'),
-            msg='No paginate() in {}'.format(impl))
-        self.assertTrue(
-            hasattr(impl, 'process'),
-            msg='No process() in {}'.format(impl))
-        self.assertTrue(
-            hasattr(impl, 'BASE_URL'),
-            msg='No BASE_URL in {}'.format(impl))
+#     def test_implementation(self):
+#         impl = self.app.get_implementation(plugin.Origin, self.BACKEND)
+#         self.assertTrue(
+#             hasattr(impl, 'paginate'),
+#             msg='No paginate() in {}'.format(impl))
+#         self.assertTrue(
+#             hasattr(impl, 'process'),
+#             msg='No process() in {}'.format(impl))
+#         self.assertTrue(
+#             hasattr(impl, 'BASE_URL'),
+#             msg='No BASE_URL in {}'.format(impl))
 
-    def test_initial_seed(self):
-        spec = plugin.OriginSpec(name='foo', backend=self.BACKEND)
-        origin = self.app.importer.get_origin_for_origin_spec(spec)
+#     def test_initial_seed(self):
+#         spec = plugin.OriginSpec(name='foo', backend=self.BACKEND)
+#         origin = self.app.importer.get_origin_for_origin_spec(spec)
 
-        g = origin.paginate(origin.BASE_URL)
-        self.assertEqual(next(g), origin.BASE_URL)
+#         g = origin.paginate(origin.BASE_URL)
+#         self.assertEqual(next(g), origin.BASE_URL)
 
-    def test_pagination(self):
-        spec = plugin.OriginSpec(name='foo', backend=self.BACKEND)
-        origin = self.app.importer.get_origin_for_origin_spec(spec)
+#     def test_pagination(self):
+#         spec = plugin.OriginSpec(name='foo', backend=self.BACKEND)
+#         origin = self.app.importer.get_origin_for_origin_spec(spec)
 
-        for (start, expected) in self.PAGINATIONS.items():
-            start = start or origin.BASE_URL
-            g = origin.paginate(start)
-            collected = []
+#         for (start, expected) in self.PAGINATIONS.items():
+#             start = start or origin.BASE_URL
+#             g = origin.paginate(start)
+#             collected = []
 
-            while len(collected) < len(expected):
-                try:
-                    collected.append(next(g))
-                except StopIteration:
-                    collected.append(None)
+#             while len(collected) < len(expected):
+#                 try:
+#                     collected.append(next(g))
+#                 except StopIteration:
+#                     collected.append(None)
 
-            self.assertEqual(collected, expected)
+#             self.assertEqual(collected, expected)
 
-    def test_processors(self):
-        for (url, n_expected) in self.URL_TESTS:
-            spec = plugin.OriginSpec(
-                name='foo', backend=self.BACKEND, url=url)
+#     def test_processors(self):
+#         for (url, n_expected) in self.URL_TESTS:
+#             spec = plugin.OriginSpec(
+#                 name='foo', backend=self.BACKEND, url=url)
 
-            srcs = self.app.importer.process_spec(spec)
-            srcs = srcs['added-sources'] + srcs['updated-sources']
-            self.assertEqual(
-                len(srcs), n_expected,
-                msg='From {}'.format(url))
-
-
-class TestEztv(TestOrigin, unittest.TestCase):
-    PLUGINS = ['eztv']
-    BACKEND = 'eztv'
-    KEYS = ['language', 'name', 'timestamp', 'type', 'uri']
-    PAGINATIONS = {
-        # Default
-        None: ['https://eztv.ag/page_{}'.format(i) for i in [0, 1, 2]],
-
-        # TV Show page
-        'http://eztv.it/shows/123/show-title/':
-            ['http://eztv.it/shows/123/show-title/', None],
-
-        # TDL change and start at page 3
-        'https://eztv.xx/page_2':
-            ['https://eztv.xx/page_{}'.format(i) for i in [2, 3]]
-
-    }
-    URL_TESTS = [
-        ('http://eztv.ag/page/0', 41)
-    ]
+#             srcs = self.app.importer.process_spec(spec)
+#             srcs = srcs['added-sources'] + srcs['updated-sources']
+#             self.assertEqual(
+#                 len(srcs), n_expected,
+#                 msg='From {}'.format(url))
 
 
-class TestKickass(TestOrigin, unittest.TestCase):
-    PLUGINS = ['kickass']
-    BACKEND = 'kickass'
-    KEYS = []
-    PAGINATIONS = {
-        # Default
-        None: ['http://kat.cr/new/?page=1'],
+# class TestEztv(TestOrigin, unittest.TestCase):
+#     PLUGINS = ['eztv']
+#     BACKEND = 'eztv'
+#     KEYS = ['language', 'name', 'timestamp', 'type', 'uri']
+#     PAGINATIONS = {
+#         # Default
+#         None: ['https://eztv.ag/page_{}'.format(i) for i in [0, 1, 2]],
 
-        # Index at 7
-        'http://kat.cr/usearch?foo=bar&page=8&lol=wow':
-            ['http://kat.cr/usearch?foo=bar&page={}&lol=wow'.format(i)
-             for i in range(8, 17)],
+#         # TV Show page
+#         'http://eztv.it/shows/123/show-title/':
+#             ['http://eztv.it/shows/123/show-title/', None],
 
-        'http://kat.cr/usearch/category%3Atv%200sec/?page=1':
-            ['http://kat.cr/usearch/category%3Atv%200sec/?page={}'.format(i)
-             for i in range(1, 5)]
-    }
-    URL_TESTS = [
-        (r'http://kat.cr/usearch/category%3Atv%200sec/', 25)
-    ]
+#         # TDL change and start at page 3
+#         'https://eztv.xx/page_2':
+#             ['https://eztv.xx/page_{}'.format(i) for i in [2, 3]]
 
-
-class TestSpanishTracker(TestOrigin, unittest.TestCase):
-    PLUGINS = ['spanishtracker']
-    BACKEND = 'spanishtracker'
-    KEYS = [
-        'language', 'leechers', 'name', 'seeds', 'size', 'timestamp',
-        'type', 'uri'
-    ]
-    PAGINATIONS = {
-        'http://spanishtracker.com/torrents.php?aaa=bbb&foo=bar&page=3':
-            ['http://spanishtracker.com/torrents.php?aaa=bbb&foo=bar&page={}'.format(i)
-             for i in [3, 4, 5]]
-    }
-    URL_TESTS = []
+#     }
+#     URL_TESTS = [
+#         ('http://eztv.ag/page/0', 41)
+#     ]
 
 
-class TestTpb(TestOrigin, unittest.TestCase):
-    PLUGINS = ['thepiratebay']
-    BACKEND = 'thepiratebay'
-    KEYS = ['leechers', 'name', 'seeds', 'size', 'timestamp', 'uri']
-    PAGINATIONS = {
-        'http://thepiratebay.com/recent/0/':
-            ['http://thepiratebay.com/recent/{}/'.format(i)
-             for i in range(2)],
+# class TestKickass(TestOrigin, unittest.TestCase):
+#     PLUGINS = ['kickass']
+#     BACKEND = 'kickass'
+#     KEYS = []
+#     PAGINATIONS = {
+#         # Default
+#         None: ['http://kat.cr/new/?page=1'],
 
-        'http://thepiratebay.com/recent/45/':
-            ['http://thepiratebay.com/recent/{}/'.format(i)
-             for i in [45, 46]],
+#         # Index at 7
+#         'http://kat.cr/usearch?foo=bar&page=8&lol=wow':
+#             ['http://kat.cr/usearch?foo=bar&page={}&lol=wow'.format(i)
+#              for i in range(8, 17)],
 
-        'http://thepiratebay.com/recent/8/b/':
-            ['http://thepiratebay.com/recent/{}/b/'.format(i)
-             for i in [8, 9]]
-    }
-    URL_TESTS = [
-        ('https://thepiratebay.am/recent', 30),
-        ('https://thepiratebay.am/search/a/0/99/0', 30)
-    ]
+#         'http://kat.cr/usearch/category%3Atv%200sec/?page=1':
+#             ['http://kat.cr/usearch/category%3Atv%200sec/?page={}'.format(i)
+#              for i in range(1, 5)]
+#     }
+#     URL_TESTS = [
+#         (r'http://kat.cr/usearch/category%3Atv%200sec/', 25)
+#     ]
 
 
-class TestTpbRss(TestOrigin, unittest.TestCase):
-    PLUGINS = ['thepiratebay']
-    BACKEND = 'tpbrss'
-    KEYS = ['name', 'size', 'timestamp', 'uri']
-    PAGINATIONS = {}
-    URL_TESTS = []
+# class TestSpanishTracker(TestOrigin, unittest.TestCase):
+#     PLUGINS = ['spanishtracker']
+#     BACKEND = 'spanishtracker'
+#     KEYS = [
+#         'language', 'leechers', 'name', 'seeds', 'size', 'timestamp',
+#         'type', 'uri'
+#     ]
+#     PAGINATIONS = {
+#         'http://spanishtracker.com/torrents.php?aaa=bbb&foo=bar&page=3':
+#             ['http://spanishtracker.com/torrents.php?aaa=bbb&foo=bar&page={}'.format(i)
+#              for i in [3, 4, 5]]
+#     }
+#     URL_TESTS = []
+
+
+# class TestTpb(TestOrigin, unittest.TestCase):
+#     PLUGINS = ['thepiratebay']
+#     BACKEND = 'thepiratebay'
+#     KEYS = ['leechers', 'name', 'seeds', 'size', 'timestamp', 'uri']
+#     PAGINATIONS = {
+#         'http://thepiratebay.com/recent/0/':
+#             ['http://thepiratebay.com/recent/{}/'.format(i)
+#              for i in range(2)],
+
+#         'http://thepiratebay.com/recent/45/':
+#             ['http://thepiratebay.com/recent/{}/'.format(i)
+#              for i in [45, 46]],
+
+#         'http://thepiratebay.com/recent/8/b/':
+#             ['http://thepiratebay.com/recent/{}/b/'.format(i)
+#              for i in [8, 9]]
+#     }
+#     URL_TESTS = [
+#         ('https://thepiratebay.am/recent', 30),
+#         ('https://thepiratebay.am/search/a/0/99/0', 30)
+#     ]
+
+
+# class TestTpbRss(TestOrigin, unittest.TestCase):
+#     PLUGINS = ['thepiratebay']
+#     BACKEND = 'tpbrss'
+#     KEYS = ['name', 'size', 'timestamp', 'uri']
+#     PAGINATIONS = {}
+#     URL_TESTS = []
 
 
 #     def test_processing(self):
