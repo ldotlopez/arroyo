71bfe40f9fe41ded4dd4d9de96e916164cce2ae4
Luis López <ldotlopez@gmail.com>
2017-02-01 21:16:39 +0100

    tests

---
+++ tests/test_watchlist.py
@@ -0,0 +1,40 @@
+
+# -*- coding: utf-8 -*-
+
+import unittest
+
+import testapp
+from arroyo.plugins import watchlist
+
+
+class WatchlistTest(unittest.TestCase):
+    def setUp(self):
+        self.app = testapp.TestApp({
+            'plugin.watchlist.list.imdb': ''
+        })
+
+    def tearDown(self):
+        delattr(self, 'app')
+
+    def test_config(self):
+        s1 = {
+            'enabled': True,
+            'label1': 'http://foo.com/',
+            'label2': {
+                'url': 'http://bar.com/',
+                'locale': 'es_ES'
+            }
+        }
+        self.app.settings.set('plugin.watchlist', s1)
+
+    def test_parse(self):
+        w = watchlist.Watchlist(self.app)
+        with open(testapp.www_sample_path('imdb-user-watchlist.html')) as fh:
+            res = w.parse(fh.read())
+            self.assertEqual(
+                len(res),
+                92
+            )
+
+if __name__ == '__main__':
+    unittest.main()
