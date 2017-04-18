480b229902afbcdd008ee2ea99558e2967fb6020
Luis López <ldotlopez@gmail.com>
2017-04-18 20:34:51 +0200

    Add naming conventions to sqlalchemy

--- arroyo/models.py
+++ arroyo/models.py
@@ -27,6 +27,15 @@ from sqlalchemy.orm import (
 )
 
 
+sautils.Base.metadata.naming_convention = {
+  "ix": 'ix_%(column_0_label)s',
+  "uq": "uq_%(table_name)s_%(column_0_name)s",
+  "ck": "ck_%(table_name)s_%(constraint_name)s",
+  "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+  "pk": "pk_%(table_name)s"
+}
+
+
 @functools.lru_cache(maxsize=8)
 def _ensure_model_class(x):
 
