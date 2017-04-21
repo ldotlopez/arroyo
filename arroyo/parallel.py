5bbe267c9f204c23fc797bbe62c3e2a31248f2ed
Luis López <ldotlopez@gmail.com>
2017-04-21 14:13:34 +0200

    Replace tabs with spaces

--- arroyo/parallel.py
+++ arroyo/parallel.py
@@ -5,116 +5,116 @@ from itertools import chain
 
 
 def cpu_map(fn, items, n_cpus=None, bulk=False):
-	if n_cpus is None:
-		n_cpus = multiprocessing.cpu_count()
+    if n_cpus is None:
+        n_cpus = multiprocessing.cpu_count()
 
-	if n_cpus == 1:
-		if bulk:
-			results = fn(*items)
-		else:
-			results = [fn(item) for item in items]
+    if n_cpus == 1:
+        if bulk:
+            results = fn(*items)
+        else:
+            results = [fn(item) for item in items]
 
-	else:
-		with multiprocessing.Pool(n_cpus) as p:
-			if bulk:
-				items = chunkify(items, n_chunks=n_cpus)
-				results = p.starmap(fn, items)
-				results = list(chain.from_iterable(results))
-			else:
-				results = p.map(fn, items)
+    else:
+        with multiprocessing.Pool(n_cpus) as p:
+            if bulk:
+                items = chunkify(items, n_chunks=n_cpus)
+                results = p.starmap(fn, items)
+                results = list(chain.from_iterable(results))
+            else:
+                results = p.map(fn, items)
 
-	return results
+    return results
 
 
 def bulk_helper(fn, *args):
-	"""
-	Allows fn to run with star arguments
-	"""
-	ret = []
+    """
+    Allows fn to run with star arguments
+    """
+    ret = []
 
-	for arg in args:
-		ret.append(fn(arg))
+    for arg in args:
+        ret.append(fn(arg))
 
-	return ret
+    return ret
 
 def exception_catcher_helper(fn, *args, **kwargs):
-	"""
-	Safe execution of fn.
+    """
+    Safe execution of fn.
 
-	Return whatever fn returns or, in case of Exception, the exception itself.
-	SyntaxError it's a special case and is not catched
-	"""
-	try:
-		return fn(*args, **kwargs)
+    Return whatever fn returns or, in case of Exception, the exception itself.
+    SyntaxError it's a special case and is not catched
+    """
+    try:
+        return fn(*args, **kwargs)
 
-	except SyntaxError:
-		raise
+    except SyntaxError:
+        raise
 
-	except Exception as e:
-		return e
+    except Exception as e:
+        return e
 
 
 def check_result(x):
-	if isinstance(x, Exception):
-		raise x
+    if isinstance(x, Exception):
+        raise x
 
-	return x
+    return x
 
 
 def chunkify(list_, n_chunks):
-	"""
-	Split into chunks
-	"""
-	if not isinstance(n_chunks, int) or n_chunks < 1:
-		raise ValueError(n_chunks)
+    """
+    Split into chunks
+    """
+    if not isinstance(n_chunks, int) or n_chunks < 1:
+        raise ValueError(n_chunks)
 
-	chunks = []
-	chunk_size = math.ceil(len(list_) / n_chunks)
+    chunks = []
+    chunk_size = math.ceil(len(list_) / n_chunks)
 
-	for i in range(n_chunks):
-		start = i * chunk_size
-		end = (i + 1) * chunk_size
-		chunks.append(list_[start:end])
+    for i in range(n_chunks):
+        start = i * chunk_size
+        end = (i + 1) * chunk_size
+        chunks.append(list_[start:end])
 
-	return chunks
+    return chunks
 
 
 if __name__ == '__main__':
-	import time
+    import time
 
-	def _cpu_bound_func(x):
-		# Check if it's prime
-		for n in range(2, (x // 2) + 1):
-			if x % n == 0:
-				raise ValueError(x)
+    def _cpu_bound_func(x):
+        # Check if it's prime
+        for n in range(2, (x // 2) + 1):
+            if x % n == 0:
+                raise ValueError(x)
 
-		# Add some sleep
-		time.sleep(0.5)
+        # Add some sleep
+        time.sleep(0.5)
 
-		return x*x
-
-
-	def cpu_bound_func(x):
-		return exception_catcher_helper(_cpu_bound_func, x)
+        return x*x
 
 
-	def cpu_bound_func_star_compatible(*xs):
-		print ("Got {} items to process".format(len(xs)))
-		return star_args_helper(cpu_bound_func, *xs)
+    def cpu_bound_func(x):
+        return exception_catcher_helper(_cpu_bound_func, x)
 
-	# Build some arguments
-	args = range(8)
 
-	# Simpliest case:
-	results = cpu_parallelize(cpu_bound_func, args)
+    def cpu_bound_func_star_compatible(*xs):
+        print ("Got {} items to process".format(len(xs)))
+        return star_args_helper(cpu_bound_func, *xs)
 
-	# Use a multiprocessing function
-	results = cpu_parallelize(cpu_bound_func_star_compatible, args, use_star_args=True)
+    # Build some arguments
+    args = range(8)
 
-	for (arg, res) in zip(args, results):
-		try:
-			res = check_result(res)
-			print("{} * {} = {}".format(arg, arg, res))
+    # Simpliest case:
+    results = cpu_parallelize(cpu_bound_func, args)
 
-		except Exception as e:
-			print('{} not prime, raises {}'.format(arg, repr(res)))
+    # Use a multiprocessing function
+    results = cpu_parallelize(cpu_bound_func_star_compatible, args, use_star_args=True)
+
+    for (arg, res) in zip(args, results):
+        try:
+            res = check_result(res)
+            print("{} * {} = {}".format(arg, arg, res))
+
+        except Exception as e:
+            print('{} not prime, raises {}'.format(arg, repr(res)))
